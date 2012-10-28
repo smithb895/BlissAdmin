16cb470820dd48ebdfab09fd83ac520f17a8f28a
Anzu <smithb895@gmail.com>
2012-10-28 06:30:22 -0300

    Fixed GPS displaying wrong coords
    Removed "(154-...)" from line 130, was causing the wrong GPS coords to be displayed.

--- modules/info/1.php
+++ modules/info/1.php
@@ -127,7 +127,7 @@ while ($row=mysql_fetch_array($res)) {
 							</div>
 							<div class="gpstext" style="width:120px;margin-left:13px;margin-top:61px">
 							<?php
-								echo sprintf("%03d",round($Worldspace[1]/100)).sprintf("%03d",round((154-($Worldspace[2]/100))));
+								echo sprintf("%03d",round($Worldspace[1]/100)).sprintf("%03d",round(($Worldspace[2]/100)));
 							?>
 							</div>							
 						</div>
