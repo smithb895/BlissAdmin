e2e1e34304ed17f1d7f3169b9088756495f5b4e0
Bob Smith <smithb895@gmail.com>
2013-01-20 10:34:43 +0800

    Support View Online Players for All Servers in Config
    +View Online Players page should now work with all servers setup in
    config

--- modules/table.php
+++ modules/table.php
@@ -11,6 +11,23 @@ if (isset($_SESSION['user_id']))
 	$nav  = '';
 	$self = 'admin.php?view=table&show='.$show;
 	$paging = '';
+	if (isset($_GET['instance'])) {
+		if (preg_match('#[^0-9+]#', $_GET['instance'])) {
+			die("Invalid instance number");
+		} else {
+			$instance = $_GET['instance'];
+		}
+		foreach ($DayZ_Servers as $server) {
+			if ($instance == $server->getMissionInstance()) {
+				$iid = $instance;
+				$serverip = $server->getServerIP();
+				$serverport = $server->getServerPort();
+				$rconpassword = $server->getRconPassword();
+				$map = $server->getServerMap();
+				$world = $server->getWorldID();
+			}
+		}
+	}
 	
 	$serverrunning = false;
 	$delresult = "";
@@ -73,11 +90,11 @@ if (isset($_SESSION['user_id']))
 </div>
 <table border="0" width="100%" cellpadding="0" cellspacing="0" id="content-table">
 	<tr>
-		<th rowspan="3" class="sized"><img src="images/shared/side_shadowleft.jpg" width="20" height="300" alt="" /></th>
+		<th rowspan="3" class="sized"></th>
 		<th class="topleft"></th>
 		<td id="tbl-border-top">&nbsp;</td>
 		<th class="topright"></th>
-		<th rowspan="3" class="sized"><img src="images/shared/side_shadowright.jpg" width="20" height="300" alt="" /></th>
+		<th rowspan="3" class="sized"></th>
 	</tr>
 	<tr>
 		<td id="tbl-border-left"></td>
